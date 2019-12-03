b8b8db8acb3d903c1d4f02579e9748b62935ffce
berkan011 <immortalhyperion@gmail.com>
2019-12-04 01:11:38 +0300

    Updates

---
+++ precache-manifest.a78e98397447b302730c8da5188edad2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a96f78c05549dfeb0fff97e988a61a4",
+    "url": "/map/index.html"
+  },
+  {
+    "revision": "d6ee7fb45309909dd868",
+    "url": "/map/static/css/main.1a75ae5d.chunk.css"
+  },
+  {
+    "revision": "9fc21bd8a95aa5a69c98",
+    "url": "/map/static/js/2.994f90ed.chunk.js"
+  },
+  {
+    "revision": "d6ee7fb45309909dd868",
+    "url": "/map/static/js/main.d916f734.chunk.js"
+  },
+  {
+    "revision": "6ed8749026b1fe9d5d33",
+    "url": "/map/static/js/runtime-main.b1624d33.js"
+  },
+  {
+    "revision": "36984dba4b26dc183fdb2df8a9184eb0",
+    "url": "/map/static/media/green-home.36984dba.png"
+  }
+]);
